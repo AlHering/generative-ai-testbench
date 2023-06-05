1373de9cdc4fdb087cbc41e98c82b91620c9f283
Alexander Hering <dev.hering@gmail.com>
2023-06-05 18:37:20 +0200

    starting initiation method for librarian agents

--- librarian/librarian.py
+++ librarian/librarian.py
@@ -9,6 +9,12 @@
 
 class Librarian(object):
     """
-    Class, representing an LLM-based librarian Agent to interactively query document libraries.
+    Class, representing an LLM-based librarian agent to interactively query document libraries.
     """
-    pass
+
+    def __init__(self, profile: dict) -> None:
+        """
+        Initiation method.
+        :param profile: Profile, configuring a librarian agent.
+        """
+        self.profile = profile
