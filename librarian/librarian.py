5c499d94509232c0f6f6a4c710925fd652362958
Alexander Hering <dev.hering@gmail.com>
2023-06-05 17:56:56 +0200

    building out basic structure

---
+++ librarian/librarian.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+****************************************************
+*           langchain_testbench:librarian                 
+*            (c) 2023 Alexander Hering             *
+****************************************************
+"""
+
+
+class Librarian(object):
+    """
+    Class, representing an LLM-based librarian Agent to interactively query document libraries.
+    """
+    pass
