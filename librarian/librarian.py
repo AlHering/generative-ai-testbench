378b1f640b3af4aecbfd1ff54620cf9cd696b3c7
Alexander Hering <dev.hering@gmail.com>
2023-06-16 11:32:16 +0200

    restructured document adding

--- librarian/librarian.py
+++ librarian/librarian.py
@@ -6,7 +6,7 @@
 ****************************************************
 """
 import os
-from typing import List
+from typing import List, Tuple
 from langchain.docstore.document import Document
 from multiprocessing import Pool
 from tqdm import tqdm
@@ -24,6 +24,8 @@ class Librarian(object):
         :param profile: Profile, configuring a librarian agent.
         """
         self.profile = profile
+        self.chroma_db = langchain_utility.get_or_create_chromadb(
+            profile["db"])
 
     def reload_folder(self, folder: str) -> None:
         """
@@ -31,7 +33,7 @@ class Librarian(object):
         :param folder: Folder path.
         """
         documents = []
-        contents = []
+        content_batches = []
         for root, dirs, files in os.walk(folder, topdown=True):
             for file in files:
                 _, ext = os.path.splitext(file)
@@ -39,10 +41,12 @@ class Librarian(object):
                     documents.append((os.path.join(root, file), ext))
         with Pool(processes=os.cpu_count()) as pool:
             with tqdm(total=len(documents), desc='(Re)loading folder contents...', ncols=80) as progress_bar:
-                for i, document_contents in enumerate(pool.imap_unordered(self.reload_document, documents)):
-                    contents.extend(document_contents)
-                    progress_bar.update(i)
-        self.add_contents_to_db(contents)
+                for index, document_contents in enumerate(pool.imap_unordered(self.reload_document, documents)):
+                    content_batches.append(document_contents)
+                    progress_bar.update(index)
+        contents, metadata_entries = self.enrich_content_batches(
+            content_batches)
+        self.add_contents_to_db(contents, metadata_entries)
 
     def reload_document(self, document_path: str, extension: str = None) -> List[Document]:
         """
@@ -53,9 +57,27 @@ class Librarian(object):
         """
         return langchain_utility.DOCUMENT_LOADERS[extension if extension is not None else os.path.splitext(document_path)[1]](document_path).load()
 
-    def add_contents_to_db(self, document_contents: List[str]) -> None:
+    def enrich_content_batches(self, file_paths: List[str], content_batches: List[list]) -> Tuple[list, list]:
+        """
+        Method for creating metadata for file contents.
+        :param file_paths: File paths.
+        :param content_batches: Content batches.
+        :return: Metadata-enriched contents.
+        """
+        contents = []
+        metadata_entries = []
+        for file_index, document_contents in enumerate(content_batches):
+            for part_index, document_part in document_contents:
+                contents.append(document_part)
+                metadata_entries.append({"file_path": file_paths[file_index],
+                                         "part": part_index})
+        return contents, metadata_entries
+
+    def add_contents_to_db(self, document_contents: List[str], document_metadata: List[str]) -> None:
         """
         Method for adding document contents to DB.
         :param document_contents: Document contents.
+        :param document_metadata: Metadata entries.
         """
-        pass
+        langchain_utility.add_documents_to_chromadb(
+            self.chroma_db, document_contents, document_metadata)
