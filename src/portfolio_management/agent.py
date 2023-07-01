9aea9b07fbc2e35c25c9e46fbcb56a98bab0340f
Alexander Hering <dev.hering@gmail.com>
2023-07-01 16:17:38 +0200

    basing agents off of abstract class

---
+++ src/portfolio_management/agent.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+****************************************************
+*           generative_ai_testbench:librarian                 
+*            (c) 2023 Alexander Hering             *
+****************************************************
+"""
+import abc
+
+
+class Agent(abc.ABC):
+    """
+    Class, representing an Agent inside the Portfolio Mangement context.
+    """
+    pass
