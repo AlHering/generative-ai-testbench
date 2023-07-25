2f444ee699cf7beec7efd798c5afcf936bf1a6e1
Alexander Hering <dev.hering@gmail.com>
2023-07-25 16:54:44 +0200

    overriding enrichement and added raw text

--- src/organizer/organizer.py
+++ src/organizer/organizer.py
@@ -5,6 +5,7 @@
 *            (c) 2023 Alexander Hering             *
 ****************************************************
 """
+from typing import List, Tuple
 from src.librarian.librarian import Librarian
 
 
@@ -25,6 +26,24 @@ class Organizer(Librarian):
                 'source_chunks': Source chunks.
         """
         super().__init__(profile)
+
+    # Override
+    def enrich_content_batches(self, file_paths: List[str], content_batches: List[list]) -> Tuple[list, list]:
+        """
+        Method for creating metadata for file contents.
+        :param file_paths: File paths.
+        :param content_batches: Content batches.
+        :return: Metadata-enriched contents.
+        """
+        contents = []
+        metadata_entries = []
+        for file_index, document_contents in enumerate(content_batches):
+            for part_index, document_part in document_contents:
+                contents.append(document_part)
+                metadata_entries.append({"file_path": file_paths[file_index],
+                                         "part": part_index,
+                                         "raw": document_part})
+        return contents, metadata_entries
 
     def run_clustering(self) -> None:
         """
