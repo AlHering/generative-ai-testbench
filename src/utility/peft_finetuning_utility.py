fd9f6a6a9f2f6256ffdb3e8fc0ba1f6c4e360dff
Alexander Hering <dev.hering@gmail.com>
2023-07-26 13:14:38 +0200

    added imports

--- src/utility/peft_finetuning_utility.py
+++ src/utility/peft_finetuning_utility.py
@@ -5,3 +5,10 @@
 *            (c) 2023 Alexander Hering             *
 ****************************************************
 """
+import os
+import torch
+from torch.utils.data import DataLoader
+from transformers import AutoTokenizer
+from transformers import AutoModelForSeq2SeqLM
+from transformers import default_data_collator, get_linear_schedule_with_warmup
+from peft import PeftModel, PeftConfig
